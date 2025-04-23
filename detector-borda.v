8de171869be58391610ff4a47e58f6ed4657af81
mouzarthy <mouzarthy2007@hotmail.com>
2025-04-23 18:41:02 -0300

    detector de bordas 2

--- detector-borda.v
+++ detector-borda.v
@@ -5,22 +5,23 @@ module detector_borda (
     output reg[1:0] detector
 );
 
+reg [1:0] entrada_anterior;
+
 always @(posedge clk or posedge rst) begin
-    if(rst) 
-    begin
-        detector = 2'b00;
-    end 
-    else 
-    begin
-    if(entrada == 2'b00)
-        detector = 2'b00;
-    else if(entrada == 2'b01)
-        detector = 2'b01;
-    else if(entrada == 2'b10)
-        detector = 2'b10;
-    else if(entrada == 2'b11)
-        detector = 2'b11;
+    if (rst) begin
+        entrada_anterior <= 2'b00;
+        detector <= 2'b00;
+    end else begin
+        if (entrada == 2'b11 && entrada_anterior == 2'b00) begin
+            detector <= 2'b11; // Detecta borda de subida em ambos os bits
+        end else if (entrada[0] == 1'b1 && entrada_anterior[0] == 1'b0 && entrada[1] == entrada_anterior[1]) begin
+            detector <= {1'b1, 1'b0}; // Detecta borda de subida no bit 0
+        end else if (entrada[1] == 1'b1 && entrada_anterior[1] == 1'b0 && entrada[0] == entrada_anterior[0]) begin
+            detector <= {1'b0, 1'b1}; // Detecta borda de subida no bit 1
+        end else begin
+            detector <= 2'b00;
+        end
+        entrada_anterior <= entrada;
     end
 end
-
 endmodule
